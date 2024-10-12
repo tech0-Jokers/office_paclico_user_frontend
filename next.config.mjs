2dc38225b7cd78c20c3894518db2bb812378805d
ryosan-7 <30883561+ryoakira12345@users.noreply.github.com>
2024-10-12 23:32:57 +0900

    Update next.config.mjs to add Cloudinary as an allowed image domain and set output to "standalone"

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
     domains: ["res.cloudinary.com"], // Cloudinary のホストを追加
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
