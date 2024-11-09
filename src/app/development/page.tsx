f78736b180c599f6b1953ba3229832bc3a51caba
ryosan-7 <30883561+ryoakira12345@users.noreply.github.com>
2024-11-09 21:49:42 +0900

    CandyShopコンポーネントを削除し、ChocolateShopコンポーネントを表示するように変更

--- src/app/development/page.tsx
+++ src/app/development/page.tsx
@@ -1,13 +1,8 @@
-import CandyShop from "@/components/CandyShop"; // コンポーネントのインポート
 import ChocolateShop from "@/components/ChocolateShop"; // コンポーネントのインポート
 
 export default function HomePage() {
   return (
     <div>
-      <div>
-        <h1>Welcome to the Candy Shop</h1>
-        <CandyShop /> {/* CandyShop コンポーネントを表示 */}
-      </div>
       <div>
         <h1>Welcome to the Chocolate Shop</h1>
         <ChocolateShop /> {/* ChocolateShop コンポーネントを表示 */}
